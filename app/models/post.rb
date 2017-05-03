e59dfe165423f42ca3cc586b7a2dbecada8bd078
kylewstewart <kyle.w.stewart@mac.com>
2017-05-03 13:21:50 -0400

    yeah

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction","Non-Fiction"]}
+  validate :clickbait_y
+
+
+  def clickbait_y
+    if title
+      clickbait = ["Won't Believe", "Secret", /(T|t)op[\d]+/, "Guess"]
+      # binding.pry
+      if !clickbait.collect {|click| title.match(click) == nil ? false : true }.include?(true)
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
+  end
+
+  binding.pry
+
+
 end
